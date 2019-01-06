ce74b8f1e6fd807edd5b6e46bf7004b0a002b571
patrickconnors23 <patconnors23@gmail.com>
2019-01-06 17:50:05 -0500

    Initial data preprocessing

--- scripts/helpers.py
+++ scripts/helpers.py
@@ -5,6 +5,13 @@ def getDateStr():
     now = datetime.datetime.now()
     return f"{now.month}/{now.day}/{now.year}"
 
+
+def getRates():
+    print(f"Getting exchange rates for tickets")
+    currencies = ['EUR', 'AUD', 'GBP', 'NZD', 'MXN', 'CAD', 'USD']
+    converter = CurrencyConverter()
+    return {x: converter.convert(1, currency=x, new_currency="USD") for x in currencies}
+
 def convertToDollars(amt, currency):
     if currency == "USD":
         return amt
@@ -13,3 +20,18 @@ def convertToDollars(amt, currency):
     else:
         converter = CurrencyConverter()
         return converter.convert(amt, currency, "USD")
+
+
+def makeDate(date):
+    x = date.split("-")
+    return datetime.datetime(year=int(x[0]), month=int(x[1]), day=int(x[2]))
+
+def getDaysBetween(date1, date2):
+    date1, date2 = makeDate(date1), makeDate(date2)
+    return (date2 - date1).days
+
+def getDayOfWeek(date):
+    days = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 
+        4: "Friday", 5: "Saturday", 6: "Sunday"}
+    date = makeDate(date)
+    return days[date.weekday()]
--- scripts/main.py
+++ scripts/main.py
@@ -5,31 +5,18 @@ from math import isnan
 from currency_converter import CurrencyConverter
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelBinarizer, StandardScaler
 
-from helpers import convertToDollars
+from helpers import getRates, convertToDollars, getDaysBetween, getDayOfWeek
 
 class PredictPrice():
     def __init__(self, modelPath="models/linReg.csv"):
-        self.SC_data = pd.read_pickle("data/SCFollowers.pkl")
+        self.followerData = pd.read_csv("data/SCFollowers.csv", index_col=0)
         self.eventData = pd.read_csv("data/eventDF.csv", index_col=0)
         self.idMap = pd.read_csv("data/platformMapDf.csv", index_col=0)
-        self.currencies = self.getRates()
+        self.currencies = getRates()
+        self.processedData = self.preProcess()
         tqdm.pandas()
-    
-    def getRates(self):
-        print(f"Getting exchange rates for tickets")
-        currencies = ['EUR', 'AUD', 'GBP', 'NZD', 'MXN', 'CAD', 'USD']
-        converter = CurrencyConverter()
-        return {x: converter.convert(1, currency=x, new_currency="USD") for x in currencies}
-
-    def trainTestSplit(self):
-        pass
-    
-    def scaleData(self):
-        pass
-    
-    def splitVenue(self):
-        pass
 
     def splitPrice(self, priceDict):
         priceDict = priceDict.replace("None", "\"\"")
@@ -48,31 +35,76 @@ class PredictPrice():
         return query.iloc[0]["scID"]
 
     @staticmethod
-    def toStringID(ID):
-        return "" if isnan(ID) else str(int(ID))
+    def buildXYTestTrain(df, y):
+        df = df.loc[df[y].notnull()]
+        splitSet = lambda d, l: (d.drop(columns=[l]), d[l])
+        train, test = train_test_split(df)
+        return splitSet(train, y), splitSet(test, y)
 
+    @staticmethod
+    def encodeBinCols(df, cols):
+        for col in cols:
+            enc = LabelBinarizer()
+            X = df[col]
+            data = enc.fit_transform(X)
+            newCols = pd.DataFrame(data, columns=enc.classes_  )
+            df[newCols.columns] = newCols 
+            df = df.drop(columns=[col])
+        return df
+
+    @staticmethod
+    def standardizeCols(df, cols):
+        colsToScale = df[cols]
+        scaler = StandardScaler()
+        df[cols] = scaler.fit_transform(colsToScale)
+        return df
+    
     def preProcess(self):
         # Get relevant columns from event data
         events = self.eventData[["artistID", "date", "dateAccessed", "id", "platform", "price", "venueName", "city"]]
 
         # format price
         events[["minPrice", "maxPrice"]] = events["price"].apply(self.splitPrice)
+        # Filter for events that haven't been priced yet
+        events = events.loc[events["minPrice"] != ""]
         events = events.drop(columns=["price"])
 
-        events = events.iloc[10:100]
-
         # Convert sgId to string
-        self.idMap["SG_id"] = self.idMap["SG_id"].apply(self.toStringID)
+        toStringID = lambda ID: "" if isnan(ID) else str(int(ID))
+        self.idMap["SG_id"] = self.idMap["SG_id"].apply(toStringID)
 
         # Get scID
         events["scID"] = events[["platform", "artistID"]].apply(self.getSCID, axis=1)
-        
-        # get followers
-        print(events)
-        return events
+
+        # get num followers
+        events = pd.merge(events, self.followerData, how="left", on="scID")
+        events = events.drop(columns=["Name"])
+
+        # create days until concert column  
+        events["daysUntilConcert"] = events.apply(lambda x: getDaysBetween(x["dateAccessed"], x["date"]), axis=1)
+        events["dayOfWeek"] = events["date"].apply(getDayOfWeek)
+
+        # Process categorical columns
+        events = self.encodeBinCols(events, ["dayOfWeek", "city", "platform", "venueName"])
+
+        # Scale Columns
+        numFollowersCols = list(events.filter(regex="numFollowers").columns)
+        events = self.standardizeCols(events, numFollowersCols+["daysUntilConcert"])
+
+        # Drop unneeded cols
+        events = events.drop(columns=["date", "dateAccessed", "maxPrice", "scID"])
+
+        # Split into testing and training
+        (xTrain, yTrain), (xTest, yTest) = self.buildXYTestTrain(events, "minPrice") 
+
+        return {
+            "xTrain": xTrain,
+            "yTrain": yTrain,
+            "xTest": xTest,
+            "yTest": yTest
+        }
 
 
 
 if __name__ == "__main__":
     pp = PredictPrice()
-    pp.preProcess()
